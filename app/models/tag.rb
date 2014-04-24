d6a2da86487d4743eaf18b36d9f0b92c9fd93b2e
Matt Light <matt.light@lightdatasys.com>
2014-04-23 23:27:54 -0700

    Add has_many :through associations to TimeLog and Tag objects

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,3 @@
 class Tag < ActiveRecord::Base
+  has_many :time_logs, through: :tags_time_logs
 end
--- app/models/time_log.rb
+++ app/models/time_log.rb
@@ -1,4 +1,5 @@
 class TimeLog < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+  has_many :tags, through: :tags_time_logs
 end
