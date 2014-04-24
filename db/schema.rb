6bbadf4c014a9d1c0249836785f9afcea7bed618
Matt Light <matt.light@lightdatasys.com>
2014-04-23 22:26:01 -0700

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140424051956) do
+ActiveRecord::Schema.define(version: 20140424052343) do
+
+  create_table "categories", force: true do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.integer  "time_increment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "categories", ["user_id"], name: "index_categories_on_user_id"
 
   create_table "tags", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "time_logs", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.string   "description"
+    t.date     "date"
+    t.integer  "rounded_minutes"
+    t.integer  "actual_seconds"
+    t.boolean  "timer_enabled"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "time_logs", ["category_id"], name: "index_time_logs_on_category_id"
+  add_index "time_logs", ["user_id"], name: "index_time_logs_on_user_id"
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
