6373e849e93144cb1905866ace045646d878b325
Matt Light <matt.light@lightdatasys.com>
2014-04-28 20:01:16 -0700

    Update API controller route documentation

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,13 +1,17 @@
 class Api::V1::CategoriesController < ApplicationController
+  # GET /api/v1/categories
   def index
   end
 
+  # POST /api/v1/categories
   def create
   end
 
+  # PUT /api/v1/categories/:id
   def update
   end
 
+  # DELETE /api/v1/categories/:id
   def delete
   end
 end
--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::TagsController < ApplicationController
-  # GET /api/v1/users/:user_id/tags.json
+  # GET /api/v1/tags.json
   def index
     @tags = Tag.all.user(get_current_user.id)
   end
