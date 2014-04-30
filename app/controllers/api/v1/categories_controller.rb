3bb992a821c179bc05a074d0954b5568a9323f82
Matt Light <matt.light@lightdatasys.com>
2014-04-29 17:53:20 -0700

    category_params is not reusable
    update and create require permitting differing params

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::CategoriesController < ApplicationController
 
   # PUT /api/v1/categories/:id
   def update
-    @category.update(category_params)
+    @category.update(params.permit(:title, :time_increment))
     @category
   end
 
@@ -24,10 +24,5 @@ class Api::V1::CategoriesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find_by! id: params[:id], user_id: get_current_user.id
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def category_params
-      params.permit(:title, :time_increment)
     end
 end
