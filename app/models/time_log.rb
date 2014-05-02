0aad0ff1ebd4a470789c3beea9e86cd79d7ee605
Matt Light <matt.light@lightdatasys.com>
2014-05-01 21:12:29 -0700

    Add :user scope to TimeLog

--- app/models/time_log.rb
+++ app/models/time_log.rb
@@ -3,4 +3,7 @@ class TimeLog < ActiveRecord::Base
   belongs_to :category
   has_many :tags_time_logs
   has_many :tags, through: :tags_time_logs
+  scope :user, ->(id) {
+    where(:user_id => id)
+  }
 end
