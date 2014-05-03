a06482867c64ca725b7887ca0f6e6038c2b174ac
Matt Light <matt.light@lightdatasys.com>
2014-05-02 17:53:20 -0700

    Implement 'POST /api/v1/time_logs'

--- app/controllers/api/v1/time_logs_controller.rb
+++ app/controllers/api/v1/time_logs_controller.rb
@@ -4,6 +4,13 @@ class Api::V1::TimeLogsController < ApplicationController
   end
 
   def create
+    @time_log = TimeLog.new(params.permit(:user_id, :category_id, :description, :date, :rounded_minutes, :actual_seconds, :timer_enabled))
+
+    if @time_log.save
+      render json: @time_log, status: :created
+    else
+      render json: @time_log.errors, status: :unprocessable_entity
+    end
   end
 
   def update
