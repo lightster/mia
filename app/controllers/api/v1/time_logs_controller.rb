8fdb60f820c9a4821c7ce3a896e023bac01143c3
Matt Light <matt.light@lightdatasys.com>
2014-05-06 17:26:41 -0700

    Update tags when updating time log

--- app/controllers/api/v1/time_logs_controller.rb
+++ app/controllers/api/v1/time_logs_controller.rb
@@ -35,6 +35,11 @@ class Api::V1::TimeLogsController < ApplicationController
         :actual_seconds,
         :timer_enabled
     )
+    tags = params[:tags].collect do |tag_title|
+        Tag.where(title: tag_title).first_or_create
+    end
+    @time_log.tags = tags
+
     if @time_log.update(time_log_params)
       render json: @time_log, status: :ok
     else
