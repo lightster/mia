253e828bc656de55021c6bdb8848fdf6b5bfde9a
Matt Light <matt.light@illuminateed.com>
2015-05-11 06:54:39 -0700

    Add api/v1/timer router

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var timerApi = require('./routes/api/v1/timer');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/api/v1/timer', timerApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
