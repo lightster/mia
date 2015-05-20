f0d09e063d50918cdb73850c5ff8e3f4b65ddb79
Matt Light <matt.light@illuminateed.com>
2015-05-20 01:22:57 -0700

    Add unimplemented POST /users call

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json('respond with a resource');
+router.post('/', function(req, res, next) {
+  var username = req.params.username;
+  var password = req.params.password;
+  var email = req.params.email;
+
+  if (!username) {
+    next(new Error("A username is required."));
+    return;
+  }
+  if (!password) {
+    next(new Error("A password is required."));
+    return;
+  }
+  if (!email) {
+    next(new Error("An email address is required."));
+    return;
+  }
+
+  res.json({
+    userId: 1,
+    username: username,
+    password: password,
+    email: email
+  });
 });
 
 module.exports = router;
